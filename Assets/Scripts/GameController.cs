bf2643d601906525649980ac08feff94728268df
Lenakeiz <andrea.castegnaro@outlook.com>
2018-02-08 18:31:12 +0000

    Starting to refactor GameController, singleton Pattern to be shared between scenes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,20 +4,10 @@ using System.Collections;
 
 using MadLevelManager;
 
-public class GameController {
-
-    #region singleton
-    private static readonly GameController instance = new GameController();
-    public static GameController Instance
-    {
-        get
-        {
-            return instance;
-        }
-    }
-    #endregion
+public class GameController : Singleton<GameController> {
 
 
+    //Andrea: This should be eliminated 
     private State[] m_States = new State[]{
         StateIdle.Instance,
         StateSplash.Instance,
@@ -41,6 +31,7 @@ public class GameController {
         StatePinball,
 		StateReward
     };
+
     private State m_CurState;
     private bool m_Initializtion = false;
 
@@ -61,52 +52,6 @@ public class GameController {
             ListenIn.Logger.Instance.Log(ex.Message, ListenIn.LoggerMessageType.Error);
         }
 
-    }
-
-    //DEPRECATED
-    public void Init()
-    {
-		if (!m_Initializtion)
-        {
-            m_Initializtion = true;
-            Application.targetFrameRate = 60;
-
-            //Setting the logger
-            GameObject go = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/UI/UI_Canvas_Debug"));
-            Text debug_text = go.GetComponentInChildren<Text>();
-            ListenIn.Logger.Instance.SetLoggerUIFrame(debug_text);
-            ListenIn.Logger.Instance.SetLoggerLogToExternal(true);
-            ListenIn.Logger.Instance.Log("Log started", ListenIn.LoggerMessageType.Info);
-
-            try
-            {
-                Debug.LogError("GameController: initializing database (is this a double initialization?)");
-                DatabaseXML.Instance.InitializeDatabase();
-                UploadManager.Instance.Initialize();
-                CUserTherapy.Instance.LoadDataset_UserProfile();
-                StateJigsawPuzzle.Instance.OnGameLoadedInitialization();                               
-
-                IMadLevelProfileBackend backend = MadLevelProfile.backend;
-                string profile = backend.LoadProfile(MadLevelProfile.DefaultProfile);
-                ListenIn.Logger.Instance.Log(string.Format("GameController: Loaded profile: {0}", profile), ListenIn.LoggerMessageType.Info);
-                //Debug.Log(profile);
-                
-                GameStateSaver.Instance.Load();
-                
-                ChangeState(States.Idle);
-                //ChangeState(States.Splash);
-            }
-            catch (System.Exception ex)
-            {
-                ListenIn.Logger.Instance.Log(ex.Message, ListenIn.LoggerMessageType.Error);
-            }
-            
-        }
-        else
-        {
-            ChangeState(States.Idle);
-        }
-        
     }
 
     public void Update()
