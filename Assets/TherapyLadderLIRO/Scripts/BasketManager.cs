627273178614d4b1c32386962417b91a1734e8e6
Lenakeiz <andrea.castegnaro@outlook.com>
2018-06-13 19:56:36 +0100

    Added basket selection scene with baskets, starting to add logic to the selection

---
+++ Assets/TherapyLadderLIRO/Scripts/BasketManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BasketManager : MonoBehaviour {
+
+    private List<int> selectedBasket;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
