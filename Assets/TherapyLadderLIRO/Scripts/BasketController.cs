4247b491467ec5db1aaea7dc29ea28ffabaf3534
Lenakeiz <andrea.castegnaro@outlook.com>
2018-06-14 16:02:17 +0100

    Added logic to control the baskets

---
+++ Assets/TherapyLadderLIRO/Scripts/BasketController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BasketController : MonoBehaviour {
+
+    public int m_basketNumber;
+
+    public Color m_NormalColor;
+    public Color m_HighlightColor;
+
+    public Image m_Image;
+
+	// Use this for initialization
+	void Start () {
+        m_Image = GetComponent<Image>();
+    }
+
+    public void SetHighlightColor(bool highlight)
+    {
+        if (highlight)
+        {
+            m_Image.color = m_HighlightColor;
+        }
+        else
+        {
+            m_Image.color = m_NormalColor;
+        }
+    }
+
+}
--- Assets/TherapyLadderLIRO/Scripts/BasketManager.cs
+++ Assets/TherapyLadderLIRO/Scripts/BasketManager.cs
@@ -4,15 +4,38 @@ using System.Collections.Generic;
 
 public class BasketManager : MonoBehaviour {
 
-    private List<int> selectedBasket;
+    private List<int> m_selectedBasket;
+    public int max_number_of_selected_basket = 2;
+
+    public int currentSelectedBaskets;
 
 	// Use this for initialization
 	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        m_selectedBasket = new List<int>();
+    }
+
+    void Update()
+    {
+        currentSelectedBaskets = m_selectedBasket.Count;
+    }
+
+    public void RegisterBasket(BasketController bc)
+    {
+        int indexInList = m_selectedBasket.IndexOf(bc.m_basketNumber);
+        if (indexInList != -1)
+        {
+            //Remove
+            bc.SetHighlightColor(false);
+            m_selectedBasket.RemoveAt(indexInList);
+        }
+        else
+        {
+            if (m_selectedBasket.Count < max_number_of_selected_basket)
+            {
+                bc.SetHighlightColor(true);
+                m_selectedBasket.Add(bc.m_basketNumber);
+            }                    
+        }
+    }
+
 }
