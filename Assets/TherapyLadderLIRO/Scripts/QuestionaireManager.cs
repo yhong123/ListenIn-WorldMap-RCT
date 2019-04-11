1239681f1cf42da8ce2432dc3a21fb2812afe3e4
Lenakeiz <andrea.castegnaro@outlook.com>
2019-04-11 19:37:40 +0100

    Added Functional questionaire scene. Need to check the ending and wire it to the main LIRO loop.

---
+++ Assets/TherapyLadderLIRO/Scripts/QuestionaireManager.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public enum QuestionType { InputText, Slider};
+
+public class QuestionStructure
+{
+    public QuestionType qt;
+    public string text;
+    public Text uiText;
+}
+
+public class QuestionaireManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<QuestionStructure> listQS = new List<QuestionStructure>();
+
+    [SerializeField]
+    private GameObject inputTextGO;
+
+    [SerializeField]
+    private GameObject sliderGO;
+
+    [SerializeField]
+    private GameObject thankyouGO;
+
+    [SerializeField]
+    private Text displayTextQuestion;
+
+    [SerializeField]
+    private Text displayTextSlider;
+
+    [SerializeField]
+    private InputField inputText;
+
+    [SerializeField]
+    private Slider slider;
+
+    private int questionsTotal;
+    private int questionsCounter = 0;
+
+    private List<string> responses = new List<string>();
+    private string formatResponse = "q{0}:{1}";
+    private string currResponse = "";
+
+    void Start()
+    {
+        inputTextGO.SetActive(false);
+        sliderGO.SetActive(false);
+        listQS.Add(new QuestionStructure { qt = QuestionType.InputText, text = "Please describe your speech comprehension skills below", uiText = displayTextQuestion});
+        listQS.Add(new QuestionStructure { qt = QuestionType.Slider   , text = "Please rate your speech comprehension skills below"    , uiText = displayTextSlider });
+        questionsTotal = listQS.Count;
+        PrepareNextQuestion();
+    }
+
+    public void PrepareNextQuestion()
+    {
+        if (questionsCounter == questionsTotal)
+        {
+            //End of questions.
+            Debug.Log("Reached end of the questionaire");
+            SaveToFile();
+            return;
+        }
+
+        QuestionStructure currQuest = listQS[questionsCounter];
+
+        currQuest.uiText.text = currQuest.text;
+        currResponse = "";
+
+        switch (currQuest.qt)
+        {
+            case QuestionType.InputText:
+                inputTextGO.SetActive(true);
+                break;
+            case QuestionType.Slider:
+                sliderGO.SetActive(true);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void ClearCurrentQuestion()
+    {
+        slider.value = 5;
+        inputText.text = "";
+        
+    }
+
+    private void SaveResponse()
+    {
+        responses.Add(string.Format(formatResponse,questionsCounter.ToString(),currResponse));
+        questionsCounter++;
+    }
+
+    private void SaveToFile()
+    {
+        string directory = GlobalVars.GetPathToLIROOutput(NetworkManager.UserId);
+        string filename = string.Format("Questionaire_{0}.txt", TherapyLIROManager.Instance.GetCurrentTherapyCycle());
+        string fullPath = Path.Combine(directory, filename);
+        try
+        {
+            using (StreamWriter sw = System.IO.File.CreateText(fullPath))
+            {
+                foreach (string line in responses)
+                {
+                    sw.WriteLine(line);
+                }
+                sw.Close();
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+    public void OnNextClickedSlider()
+    {
+        currResponse = slider.value.ToString();
+        SaveResponse();
+        PrepareNextQuestion();
+    }
+
+    public void OnNextClickedQuestionaire()
+    {
+        currResponse = inputText.text;
+        SaveResponse();
+        PrepareNextQuestion();
+    }
+
+}
