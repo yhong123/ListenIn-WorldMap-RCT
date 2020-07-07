f591275f8520a3c08bc1df604060c63244ba16e8
Pedro <pedro@quiley.com>
2020-07-07 20:05:46 +0100

    added concent form on login

---
+++ Assets/Scripts/Login/ConcentFormEnableContinue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConcentFormEnableContinue : MonoBehaviour
+{
+	[SerializeField] private Toggle toggle1;
+	[SerializeField] private Toggle toggle2;
+	[SerializeField] private Toggle toggle3;
+	[SerializeField] private GameObject continueButton;
+
+	public void OnValueChange()
+	{
+		if(toggle1.isOn && toggle2.isOn && toggle3.isOn)
+		{
+			continueButton.SetActive(true);
+		}
+		else
+		{
+			continueButton.SetActive(false);
+		}
+	}
+}
