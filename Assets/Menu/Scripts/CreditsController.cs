6720a66abb2e86ea4c949f43c9ce14323a7d638c
Lenakeiz <andrea.castegnaro@outlook.com>
2020-12-25 22:39:27 +0000

    Added credits to the mainHub with gentleman animation and autoscrolling. Inserted TextMeshPro in the packages.

---
+++ Assets/Menu/Scripts/CreditsController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class CreditsController : MonoBehaviour {
+
+    public CanvasGroup cg;
+
+    [Header("Assigned in the inspector")]
+    public ScrollRect sc;
+    public Scrollbar sb;
+    [Range(10.0f,50.0f)]
+    public float scrollSpeed = 10.0f;
+    public float waitTime = 5.0f;
+    bool autoScrolling = false;
+
+	// Use this for initialization
+	void Start () {
+        cg = gameObject.GetComponent<CanvasGroup>();
+        if (cg == null)
+            Debug.LogError("Please assign canvas group to the credits button");
+        if(sb == null)
+            Debug.LogError("Please assign vertical scroll group to the credits button");
+
+        cg.alpha = 0.0f;
+        cg.blocksRaycasts = false;
+	}
+
+    public void HideCanvas(bool canvasStatus)
+    {
+        cg.alpha = canvasStatus ? 1.0f : 0.0f;
+        cg.blocksRaycasts = canvasStatus;
+        if (canvasStatus)
+        {
+            MainHubAnimatorController.Instance.AnimateCharacter(MainHubAnimatorController.animatorMoves.Throw);
+            StartCoroutine(AutoScrolling());            
+        } 
+        else StopScrolling(true);
+    }
+
+    public void StopScrolling(bool reset)
+    {
+        if (autoScrolling)
+        {
+            StopAllCoroutines();
+            autoScrolling = false;
+        }        
+        if(reset)
+            sb.value = 1f;
+    }
+
+    private IEnumerator AutoScrolling()
+    {
+        autoScrolling = true;
+        yield return new WaitForSeconds(waitTime);
+        while (autoScrolling)
+        {
+            sc.velocity = new Vector2(0.0f, scrollSpeed);
+            if (sb.value < 0.01f)
+                autoScrolling = false;
+            yield return new WaitForEndOfFrame();
+        }
+        autoScrolling = false;
+    }
+
+}
---
+++ Assets/Menu/Scripts/SplashScreenController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MadLevelManager;
+
+public class SplashScreenController : MonoBehaviour {
+
+    [Header("Assign in inspector")]
+    public List<Image> logos = new List<Image>();
+    public float updateTimeForLogo = 3.0f;
+    private int counter = 0;
+    private bool isChanging = false;
+	// Use this for initialization
+	void Start () {
+        foreach (var im in logos)
+        {
+            Color c = im.color;
+            c.a = 0.0f;
+            im.color = c;
+        }
+        StartCoroutine(ShowLogos(counter));
+
+    }
+
+    public void UpdateColor(Color c)
+    {
+        if(counter<logos.Count)
+            logos[counter].color = c;
+    }
+
+    public void OnCompletedFadeIn()
+    {
+        Debug.Log("Fade in completed " + counter);
+        Color c = logos[counter].color;
+        Color d = c;
+        d.a = 0.0f;
+        iTween.ValueTo(this.gameObject, iTween.Hash("from", c, "to", d, "time", updateTimeForLogo, "easetype", iTween.EaseType.easeOutCubic, "onupdate", "UpdateColor", "onupdatetarget", this.gameObject, "oncomplete", "OnCompletedFadeOut", "oncompletetarget", this.gameObject));
+
+    }
+
+    public void OnCompletedFadeOut()
+    {
+        Debug.Log("Fade out completed " + counter);
+
+        counter++;
+        StartCoroutine(ShowLogos(counter));
+    }
+
+    private IEnumerator ShowLogos(int counter)
+    {
+        yield return new WaitForEndOfFrame();
+        if (counter < logos.Count)
+        {
+            Color c = logos[counter].color;
+            Color d = c;
+            d.a = 1.0f;
+            iTween.ValueTo(this.gameObject, iTween.Hash("from", c, "to", d, "time", updateTimeForLogo, "easetype", iTween.EaseType.easeOutCubic, "onupdate", "UpdateColor", "onupdatetarget", this.gameObject, "oncomplete", "OnCompletedFadeIn","oncompletetarget",this.gameObject));
+        }
+        else
+        {
+            isChanging = true;
+            yield return new WaitForSeconds(1.0f);
+            SwitchScene();
+        }
+    }
+
+    public void Update()
+    {
+        //get fingers on screen android only 
+        int fingerCount = 0;
+
+#if UNITY_ANDROID
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+                fingerCount++;
+        }
+#endif
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+        fingerCount = Input.GetMouseButtonDown(0) ? 1 : 0;
+#endif
+
+        if (fingerCount > 0)
+        {
+            StopAnimation();
+            SwitchScene();
+        }
+    }
+
+    private void SwitchScene()
+    {
+        MadLevel.LoadLevelByName("SetupScreenDebug");
+    }
+
+    private void StopAnimation()
+    {
+        if (!isChanging)
+        {
+            StopAllCoroutines();
+            iTween.Stop(this.gameObject);
+        }        
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+}
